0e8653fcaaca259daf831da9fb2ecd8c187a4310
Stephen McMurtry <smcmurtry@gmail.com>
2016-09-29 16:15:15 -0600

    updated some of the text descriptions

--- web/js/common.js
+++ web/js/common.js
@@ -2,10 +2,10 @@
 var page_w = +d3.select('#main').style('width').slice(0,-2);
 
 var voting_systems = [
-  {label: 'First Past the Post (FPTP)', abbr: 'FPTP'},
-  {label: 'Dual Member Proportional (DMP)', abbr: 'DMP'},
-  {label: 'Mixed Member Proportional (MMP)', abbr: 'MMP'},
-  {label: 'Instant Runoff Voting (IRV)', abbr: 'IRV'}];
+  {label: 'First Past the Post', abbr: 'FPTP'},
+  {label: 'Dual Member Proportional', abbr: 'DMP'},
+  {label: 'Mixed Member Proportional', abbr: 'MMP'},
+  {label: 'Instant Runoff Voting', abbr: 'IRV'}];
 
 var provs = ['BC', 'AB', 'SK', 'MB', 'ON', 'QC', 'NB', 'NS', 'PE', 'NL', 'YT', 'NT', 'NU'];
 var parties = ['Liberal', 'Conservative', 'NDP', 'Bloc', 'Green', 'Other'];
--- web/js/index.js
+++ web/js/index.js
@@ -18,9 +18,10 @@ d3.queue()
       function update_all() {
         var sel_system = get_selected_row('#voting-system-dropdown', voting_systems);
         var sel_region = get_selected_row('#region-dropdown', regions);
-
-        d3.select('#system-title').html(sel_system);
-        d3.select('#region-title').html(sel_region);
+        var sel_system_label = voting_systems.filter(function(d) { return d.abbr == sel_system; })[0].label;
+        var sel_region_label = regions.filter(function(d) { return d.abbr == sel_region; })[0].label;
+        d3.select('#system-title').html(sel_system_label);
+        d3.select('#region-title').html(sel_region_label);
 
         blocks.update(dataz, sel_system, sel_region);
         bars.update(files[4], sel_system, sel_region);
